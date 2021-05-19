3ba00b83e525e7e35e7f405b9b76723c5f363131
lianyanjie <yanjie14@staff.sina.com>
2021-05-19 22:39:48 +0800

    日志和cookie

--- foodie-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,15 +1,36 @@
 package com.imooc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @ApiIgnore
 public class HelloController {
 
+    final static Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     public String hello(){
+        log.debug("hello world");
+        log.info("hello world");
+        log.warn("hello world");
+        log.error("hello world");
         return "hello world";
+    }
+
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo","new user");
+        session.setMaxInactiveInterval(3600);
+        session.getAttribute("userInfo");
+        session.removeAttribute("userInfo");
+        return "ok";
     }
 }
